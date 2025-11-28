cfc82a304c8b047ecb100f11c346c409bdf996ed
bernie26262 <bernhard.helbich@gmail.com>
2025-11-28 19:06:26 +0100

    Mega-Link integriert, Fehlermeldungen behoben, GPIO & Webserver angepasst

--- src/config/pins.h
+++ src/config/pins.h
@@ -1,11 +1,11 @@
 #pragma once
 
-// I2C
-#define PIN_I2C_SDA  42
-#define PIN_I2C_SCL  41
+// I2C Pins für ESP32-S3
+#define PIN_I2C_SDA 10
+#define PIN_I2C_SCL 11
 
-// DataReady vom Mega2560
-#define PIN_MEGA_DATAREADY  40
+// Mega2560 DATAREADY Leitung
+#define PIN_MEGA_DATAREADY 12
 
-// LED
+// Onboard LED (ESP32-S3 DevKitC-1)
 #define PIN_LED 48
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,21 @@
 #include <Arduino.h>
-
-#include "hal/gpio.h"
 #include "network/wifi_manager.h"
 #include "web/webserver.h"
-#include "core/mega_link.h"   // <-- Wichtig!
+#include "core/mega_link.h"
 
 void setup() {
     Serial.begin(115200);
-    delay(100);
+    delay(200);
 
-    hal_init();
-    mega_init();      // <-- richtig
+    Serial.println("===== ESP32-S3 Eisenbahn-Projekt gestartet =====");
+
     wifi_init();
     web_init();
+    mega_init();   // <-- richtig
 }
 
 void loop() {
-    mega_loop();      // <-- richtig
     wifi_loop();
     web_loop();
+    mega_loop();   // <-- richtig
 }
